410f2d04a0714098f03896aef508a417da1e90c0
Hiroto-Iizuka <oroxith@gmail.com>
2022-06-18 19:13:51 +0900

    feat:各ページへのRouterを実装

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter, Switch, Routes, Route } from "react-router-dom";
+import SignIn from "./auth/SignIn";
 import SignUp from "./auth/SignUp";
+import UserList from "./components/UserList";
 
 function App() {
   return (
     <div className="App">
-      <SignUp />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/" element={<UserList />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/auth/SignIn.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignIn = () => {
+  return (
+    <>
+      <h1>ログイン画面</h1>
+    </>
+  )
+}
+
+export default SignIn;
