21890517aa2d089b5a2996a2a75ac31aa33ba3bf
Hiroto-Iizuka <oroxith@gmail.com>
2022-07-03 23:01:50 +0900

    review: useEffectの無限ループ

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -1,26 +1,61 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth, db } from "../firebase/firebaseConfig";
 import { doc, getDoc } from "firebase/firestore";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const DashBoard = () => {
-  const [userDocument, setUserDocument] = useState('');
+
+  const [user, setUser] = useState();
+  const [userAuth, setUserAuth] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      if (typeof userAuth !== 'object') {
+        setUserAuth(currentUser);
+      }
+    });
+  }, [userAuth]);
 
   useEffect(() => {
     (async () => {
       const currentUserDocumentId = auth.lastNotifiedUid;
       const docRef = doc(db, "users", currentUserDocumentId);
       const docSnap = await getDoc(docRef);
-      setUserDocument(docSnap.data()); 
+      if (typeof user !== 'object') {
+        setUser(docSnap.data());
+      }
     })();
-  }, []);
+  }, [])
+
+  const navigate = useNavigate();
+
+  const logout = async () => {
+    await signOut(auth);
+    navigate("/signin");
+  }
 
   return (
     <>
-      <h1>ダッシュボード</h1>
-      <p>{userDocument.userName}さんようこそ!</p>
-      <p>残高：{userDocument.wallet}</p>
+      {!userAuth ? (
+        <Navigate to={"signin"} />
+      ) : (
+        <>
+          {!user ? (
+            <Navigate to={"signin"} />
+          ) : (
+            <>
+              <h1>ダッシュボード</h1>
+              <p>{user?.userName}さんようこそ!</p>
+              <p>残高：{user?.wallet}</p>
+              <button onClick={logout}>ログアウト</button>
+            </>
+          )}
+        </>
+      )}
     </>
   )
 }
+
 
 export default DashBoard;
