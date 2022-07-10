277cdafcb4b1628adbfed0a510cef4eefd7869d4
Hiroto-Iizuka <oroxith@gmail.com>
2022-07-10 20:58:27 +0900

    feat: ログアウト機能を実装

--- src/auth/SignIn.js
+++ src/auth/SignIn.js
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from "react";
-import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import React, { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase/firebaseConfig";
-import { Link, Navigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignIn = () => {
   const [signInEmail, setSignInEmail] = useState("");
   const [signInPassword, setSignInPassword] = useState("");
+
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -14,56 +16,42 @@ const SignIn = () => {
         auth,
         signInEmail,
         signInPassword,
-      )
-      window.location.href = "/";
+      );
+      navigate("/");
     } catch(error) {
       alert("メールアドレスもしくはパスワードが間違っています")
     }
   }
 
-  const [user, setUser] = useState("");
-
-  useEffect(() => {
-    onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-    });
-  }, []);
-
   return (
     <>
-      {user ? (
-        <Navigate to={"/"} />
-      ) : (
-        <>
-          <h1>ログイン画面</h1>
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label>メールアドレス</label>
-              <input
-                name="email"
-                type="email"
-                value={signInEmail}
-                onChange={(e) => setSignInEmail(e.target.value)}
-                placeholder="E-mail"
-              />
-            </div>
-            <div>
-              <label>パスワード</label>
-              <input
-                name="password" 
-                type="password"
-                value={signInPassword}
-                onChange={(e) => setSignInPassword(e.target.value)}
-                placeholder="Password" 
-              />
-            </div>
-            <div>
-              <button>ログイン</button>
-            </div>
-          </form>
-          <p><Link to={{pathname: "/signup"}}>新規登録はこちらから</Link></p>
-        </>
-      )}
+      <h1>ログイン画面</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>メールアドレス</label>
+          <input
+            name="email"
+            type="email"
+            value={signInEmail}
+            onChange={(e) => setSignInEmail(e.target.value)}
+            placeholder="E-mail"
+          />
+        </div>
+        <div>
+          <label>パスワード</label>
+          <input
+            name="password" 
+            type="password"
+            value={signInPassword}
+            onChange={(e) => setSignInPassword(e.target.value)}
+            placeholder="Password" 
+          />
+        </div>
+        <div>
+          <button>ログイン</button>
+        </div>
+      </form>
+      <p><Link to={{pathname: "/signup"}}>新規登録はこちらから</Link></p>
     </>
   )
 }
--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -1,61 +1,51 @@
-import React, { useState, useEffect, useMemo } from "react";
-import { onAuthStateChanged, signOut } from "firebase/auth";
+import React, { useState, useEffect } from "react";
+import { signOut, onAuthStateChanged } from "firebase/auth";
 import { auth, db } from "../firebase/firebaseConfig";
 import { doc, getDoc } from "firebase/firestore";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const DashBoard = () => {
 
   const [user, setUser] = useState();
-  const [userAuth, setUserAuth] = useState(null);
+  const [userAuth, setUserAuth] = useState();
+
+  const navigate = useNavigate();
 
   useEffect(() => {
-    onAuthStateChanged(auth, (currentUser) => {
-      if (typeof userAuth !== 'object') {
-        setUserAuth(currentUser);
+    // ログイン認証
+    onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        navigate("/signin");
+      } else {
+        setUserAuth(user);
       }
-    });
-  }, [userAuth]);
+    }, []);
+  });
 
   useEffect(() => {
     (async () => {
       const currentUserDocumentId = auth.lastNotifiedUid;
       const docRef = doc(db, "users", currentUserDocumentId);
       const docSnap = await getDoc(docRef);
-      if (typeof user !== 'object') {
-        setUser(docSnap.data());
-      }
+      setUser(docSnap.data());
     })();
-  }, [])
-
-  const navigate = useNavigate();
+  })
 
   const logout = async () => {
     await signOut(auth);
     navigate("/signin");
   }
 
-  return (
-    <>
-      {!userAuth ? (
-        <Navigate to={"signin"} />
-      ) : (
-        <>
-          {!user ? (
-            <Navigate to={"signin"} />
-          ) : (
-            <>
-              <h1>ダッシュボード</h1>
-              <p>{user?.userName}さんようこそ!</p>
-              <p>残高：{user?.wallet}</p>
-              <button onClick={logout}>ログアウト</button>
-            </>
-          )}
-        </>
-      )}
-    </>
-  )
+  if (userAuth !== null) {
+    return (
+      <>
+        <h1>ダッシュボード</h1>
+        <p>{user?.userName}さんようこそ!</p>
+        <p>残高：{user?.wallet}</p>
+        <button onClick={logout}>ログアウト</button>
+      </>
+    )
+  } 
 }
-
 
 export default DashBoard;
