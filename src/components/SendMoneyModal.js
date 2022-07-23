97f65fa399308eefe23f37daed9be8eb8d40cf53
Hiroto-Iizuka <oroxith@gmail.com>
2022-07-23 23:36:17 +0900

    feat: 現在のユーザーのwalletからフォームに入力した数値を減らす処理を実装

--- src/components/SendMoneyModal.js
+++ src/components/SendMoneyModal.js
@@ -1,5 +1,7 @@
 import React, { useState, useContext } from 'react';
 import { CurrentUser } from './DashBoard';
+import { db } from "../firebase/firebaseConfig";
+import { updateDoc, doc } from "firebase/firestore";
 import { 
   Button, Modal, ModalBody, ModalFooter, ModalHeader,
   Form, Label, Input,
@@ -8,24 +10,44 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const SendMoneyModal = () => {
   const currentUser = useContext(CurrentUser)
+  
   const [modal, setModal] = useState(false);
+  const [money, setMoney] = useState();
 
   const toggle = () => setModal(!modal);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    withDrawMoney();
+  }
+
+  const withDrawMoney = async () => {
+    const currentUserWallet = currentUser.wallet;
+    const resultMoney = currentUserWallet - money;
+    const currentUserRef = doc(db, "users", currentUser.uid)
+    await updateDoc(currentUserRef, {
+      wallet: resultMoney
+    });
+  }
 
   return (
     <div>
       <Button onClick={toggle}>送る</Button>
       <Modal isOpen={modal} toggle={toggle}>
         <ModalHeader>あなたの残高：{currentUser.wallet}</ModalHeader>
-        <ModalBody>
-          <Form>
+        <Form onSubmit={handleSubmit}>
+          <ModalBody>
             <Label>送る金額</Label>
-            <Input />
-          </Form>
-        </ModalBody>
-        <ModalFooter>
-          <Button>送信</Button>
-        </ModalFooter>
+            <Input 
+              id="sendMoney" 
+              value={money}
+              onChange={(e) => setMoney(e.target.value)}
+            />
+          </ModalBody>
+          <ModalFooter>
+            <Button>送信</Button>
+          </ModalFooter>
+        </Form>
       </Modal>
     </div>
   );
